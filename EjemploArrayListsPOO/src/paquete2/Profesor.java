b87e42b08cfb3d635575f80055ca2497dfa547df
Jeimy Torres <jatorres99@utpl.edu.ec>
2024-05-16 16:46:49 -0500

    cambios

--- EjemploArrayListsPOO/src/paquete2/Profesor.java
+++ EjemploArrayListsPOO/src/paquete2/Profesor.java
@@ -34,4 +34,10 @@ public class Profesor {
     public String obtenerTipo() {
         return tipo;
     }
+    @Override
+    public String toString(){
+        String cadena = String.format("%s - %s\n", obtenerNombre(), 
+                obtenerTipo());
+        return cadena;
+    }
 }
